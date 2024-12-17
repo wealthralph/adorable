e5f16adcf17f4dd7351a13d420cb8cce73da47f2
catalyst <wealthralph@gmail.com>
2024-12-17 02:09:08 +0100

    project initialization

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { HeaderMenu } from "./components/header/Header";
+
+const App = () => {
+    return (
+     <main>
+        <HeaderMenu/>
+     </main>
+    );
+    
+}
+
+export default App
