9480ce82f55d56b900c99775b65e3493c846edab
catalyst <wealthralph@gmail.com>
2024-12-17 10:21:50 +0100

    updated

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import img1 from "./IMG_3363.jpeg"
+
+
+
+export {
+    img1
+}
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,94 @@
+import { IconBrandInstagram, IconBrandTwitter, IconBrandYoutube } from '@tabler/icons-react';
+import { ActionIcon, Container, Group, Text } from '@mantine/core';
+import classes from './Footer.module.css';
+
+const data = [
+  {
+    title: 'About',
+    links: [
+      { label: 'Mission & Vision', link: '#' },
+      { label: 'Partners', link: '#' },
+      { label: 'Upcoming Events', link: '#' },
+      { label: 'Become a Volunteer', link: '#' },
+    ],
+  },
+  {
+    title: 'Project',
+    links: [
+      { label: 'Christmas Outreach', link: '#' },
+      { label: 'Medical Outreach', link: '#' },
+      { label: 'Welfare Support', link: '#' },
+    ],
+  },
+//   {
+//     title: 'Community',
+//     links: [
+//       { label: 'Join Discord', link: '#' },
+//       { label: 'Follow on Twitter', link: '#' },
+//       { label: 'Email newsletter', link: '#' },
+//       { label: 'GitHub discussions', link: '#' },
+//     ],
+//   },
+];
+
+ function Footer() {
+
+
+  const groups = data.map((group) => {
+    const links = group.links.map((link, index) => (
+      <Text
+        key={index}
+        className={classes.link}
+        component="a"
+        href={link.link}
+      >
+        {link.label}
+      </Text>
+    ));
+
+    return (
+      <div className={classes.wrapper} key={group.title}>
+        <Text className={classes.title}>{group.title}</Text>
+        {links}
+      </div>
+    );
+  });
+
+  return (
+    <footer className={classes.footer}>
+      <Container className={classes.inner}>
+        <div className={classes.logo}>
+          <Text size="xs" c="dimmed" className={classes.description}>
+            Adorable Foundation Christmas Charity Foundation{" "}
+          </Text>
+        </div>
+        <div className={classes.groups}>{groups}</div>
+      </Container>
+      <Container className={classes.afterFooter}>
+        <Text c="dimmed" size="sm">
+          © {new Date().getFullYear()} All rights reserved.
+        </Text>
+
+        <Group
+          gap={0}
+          className={classes.social}
+          justify="flex-end"
+          wrap="nowrap"
+        >
+          <ActionIcon size="lg" color="gray" variant="subtle">
+            <IconBrandTwitter size={18} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg" color="gray" variant="subtle">
+            <IconBrandYoutube size={18} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg" color="gray" variant="subtle">
+            <IconBrandInstagram size={18} stroke={1.5} />
+          </ActionIcon>
+        </Group>
+      </Container>
+    </footer>
+  );
+}
+
+
+export default Footer
